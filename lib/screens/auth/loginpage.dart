8ce151e14922c0a04eb88f24aff3665c755dc427
seggi <seggimarugira@gmail.com>
2021-12-16 22:57:52 -0500

    working on login page

---
+++ lib/screens/auth/loginpage.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:pmc_dev/widgets/custom_btn.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Colors.white24,
+          child: SingleChildScrollView(
+            child: Container(
+              child: Column(children: [
+                Container(
+                  child: Column(
+                    children: [topContent(), bottomContent()],
+                  ),
+                )
+              ]),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget topContent() {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Container(
+      color: Colors.black,
+      child: Container(
+          // padding: EdgeInsets.all(8),
+          height: height / 2.2,
+          decoration: BoxDecoration(
+            color: Colors.white24,
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(100),
+            ),
+          ),
+          child: Column(
+            children: [
+              Container(
+                  alignment: Alignment.center,
+                  child: Container(
+                    width: width,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(110),
+                        topRight: Radius.circular(120),
+                        topLeft: Radius.circular(120),
+                        bottomLeft: Radius.circular(120),
+                      ),
+                    ),
+                    //
+                  )),
+            ],
+          )),
+    );
+  }
+
+  Widget bottomContent() {
+    final height = MediaQuery.of(context).size.height;
+    final IconData btnIcon = Icons.emoji_people_outlined;
+    final IconData btnIcon1 = Icons.account_balance;
+    final Color btnColor = Colors.black12;
+    final Color btnColor1 = Colors.white12;
+    final Color btnTextColor = Colors.white60;
+    return Container(
+      color: Colors.teal,
+      child: Container(
+        height: height / 1.5,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(120),
+          ),
+        ),
+        child: Column(
+          children: [
+            Container(
+              color: Colors.white24,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(150),
+                    ),
+                    border: Border.all(color: Colors.black)),
+                padding: EdgeInsets.only(top: 55, bottom: 55),
+                alignment: Alignment.bottomCenter,
+                child: Text("Sign In",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 28,
+                        color: Colors.white)),
+              ),
+            ),
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Text("What Best Describes You?",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                      color: Colors.white)),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                customBtn(btnIcon, "Individual", btnColor, btnTextColor),
+                SizedBox(width: 20),
+                customBtn1(btnIcon1, "Company", btnColor1, btnTextColor),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 0, top: 40),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't you have an account yet?",
+                    style: TextStyle(
+                        color: Colors.white70, fontWeight: FontWeight.w600),
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "sign up",
+                        style: TextStyle(
+                            color: Colors.teal, fontWeight: FontWeight.w600),
+                      ))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
