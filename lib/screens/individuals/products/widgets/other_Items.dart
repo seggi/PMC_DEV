e1a48c15d16dfd5bb5f1cd6ae3ff1c22b1e84fec
thierrybishop <thierrybishop@gmail.com>
2022-01-19 22:02:19 +0200

    new changes

--- lib/screens/individuals/products/widgets/other_Items.dart
+++ lib/screens/individuals/products/widgets/other_Items.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pmc_dev/screens/individuals/products/consumer/convenience_goods/convenience_page.dart';
 import 'package:pmc_dev/screens/individuals/products/consumer/convenience_goods/liquor_page.dart';
